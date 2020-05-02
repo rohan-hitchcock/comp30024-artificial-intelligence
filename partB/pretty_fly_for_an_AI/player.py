27627e9521aee5cd362eb05b06cb1b56c292a70b
Patrick Randell <prandell@student.unimelb.edu.au>
2020-05-02 14:12:26 +0800

    rewrote evaluation function

--- partB/pretty_fly_for_an_AI/player.py
+++ partB/pretty_fly_for_an_AI/player.py
@@ -4,6 +4,7 @@ from pretty_fly_for_an_AI.minimax import alpha_beta_search as minimax
 from collections import defaultdict as dd
 from math import ceil
 
+
 class Player:
     minimax_depth = 3
 
@@ -14,43 +15,62 @@ class Player:
         return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
 
     @staticmethod
-    def new_eval(state):
-
+    def evaluation_function(state, weights):
         value = 0
+        for i in range(len(weights)):
+            value += weights[i] * Player.dpartial_reward(state, weights, i)
+        return value
 
-        ours = state.board[state.board > 0]
-        theirs = state.board[state.board < 0]
-        rest = state.board[state.board == 0]
-        diff = (np.sum(ours) + np.sum(theirs))
+    @staticmethod
+    def dpartial_reward(state, weights, i):
 
-        # feature 1: Number of pieces for each player
-        value += 50 * diff * diff * np.sign(diff)
-        value -= 4 * len(ours)
-        value += 4 * len(rest)
+        # using if statements to avoid calculating things we dont need
+        if i == 0:
+            # Similar to a tan function in shape, as the difference between our tokens and theirs grows, were
+            # greater encouraged to take their tokens
+            ours = state.board[state.board > 0]
+            theirs = state.board[state.board < 0]
+            diff = (np.sum(ours) + np.sum(theirs))
+            return diff * diff * np.sign(diff)
 
-        #feature: winning
-        if len(theirs) == 0:
-            value += 100000
+        elif i == 1:
+            # Should have negative weight, encourages us to stack
+            return len(state.board[state.board > 0])
 
-        # feature 2: Number of available moves
+        elif i == 2:
+            # Also encourages us to stack, but also encourages us to remove their tokens. My idea was that this might
+            # come into play when we have more tokens, but want to sacrifice 1 for 1. A move like this isnt directly
+            # rewarded
+            return len(state.board[state.board == 0])
+
+        elif i == 3:
+            # The reward for winning. Shifted inverse function. 0.1 and 10 chosen so that, for x = [1 --> 12],
+            # y = [0.01 --> 0.008] but for x = 0, y = 10. Thus if the weight is 1000, the reward for winning is
+            # 10000, but only 10 when the opposition has 1 token.
+            return ceil(1 / (0.1 + 10 * len(state.board[state.board < 0])))
+
+        # ------------------- FEATURES TO KEEP IN MIND ------------------ #
+
+        # Encourages the player to move to the centre of the board when there are little stacks remaining.
+        # I think this is important, but hard to flatten into a straight assignment. Will leave commented for now.
+        # distance = lambda index: ceil(state.board[index]*Player.manhattan(s.itop(index), (3, 4)))
+        # feat_5 = 0
+        # if len(rest) > 59:
+        #     for i in state.board:
+        #         if i > 0:
+        #             feat_5 -= 10* distance(i)
+
+        # feature: Number of available moves
         # value += w_f2 * len(list(state.next_states(opponent=False)))
         # value -= b_f2 * len(list(state.next_states(opponent=True)))
 
-        # feature 3: Distance to middle of the board. Late game
-        if len(rest) > 59:
-            for i in state.board:
-                if i > 0:
-                    value -= ceil(10*state.board[i]*Player.manhattan(s.itop(i), (3, 4)))
-
-        # feature 4: Explosion radius. Should include whose turn it is into this
+        # feature: Explosion radius. Should include whose turn it is into this
         # for i in range(64):
         #     sum = 0
         #     if state.board[i] != 0:
         #         for j in s.boom_radius(i):
         #             sum += state.board[j]
         #         value += 10 * (state.board[i] - sum)
-
-        return value
 
     def __init__(self, color):
         """
@@ -111,6 +131,6 @@ class Player:
             pos = data[0]
             self.state = self.state.boom(s.ptoi(*pos))
 
+
 if __name__ == "__main__":
     pass
-
